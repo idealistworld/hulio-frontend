a33872b8572ee4a1584372f362c5a76be04a91c2
Adrien <adriennavarre@hotmail.com>
2022-08-27 18:58:32 -0500

    main website progress

--- website/src/pages/litepaper.tsx
+++ website/src/pages/litepaper.tsx
@@ -1,13 +1,15 @@
+import LitepaperLeft from "../components/litepaper/LitepaperLeft";
 import MiddleContent from "../components/middle-content/MiddleContent";
 import Navbar from "../components/navbar/Navbar";
 import RightBox from "../components/right-box/RightBox";
+
 
 const Litepaper = () =>{
     return (
         <div className="home-container">
             <Navbar />
             <div className="home-content-container">
-                    <Litepaper />
+                    <LitepaperLeft />
                     <RightBox />
             </div>
         </div>
