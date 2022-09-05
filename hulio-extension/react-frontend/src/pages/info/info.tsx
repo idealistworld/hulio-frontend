350f6ddd84d952b6106cd5cdfe70fa6e89fb05f0
Adrien <adriennavarre@hotmail.com>
2022-09-04 23:23:56 -0500

    info page converted

--- hulio-extension/react-frontend/src/pages/info/info.tsx
+++ hulio-extension/react-frontend/src/pages/info/info.tsx
@@ -1,25 +1,46 @@
-
+import api from '../../utils/api';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom'
 import './info.css'
 
 export default function Info() {  
+    let { url } = useParams();
+
+    const addressName = "Hulio Team"
+    const address = "2v155UE19ghxH5CoUpBZQwjtYKHR9g4z7eS5qhPgJxTy"
+    let tx_hash:string = '', date_created:any = null;
+
+    const getData = async () =>{
+        const response = await api.get(`/website/get_website/${url}`);
+
+        console.log(response)
+
+        // tx_hash = response.tx_hash;
+        // date_created = response.date_created;
+    }
+
+    useEffect(() =>{
+        getData()
+    }, []);
+
     return (
         <div className="info-container">
             <div className="content">
-                <a id='projectTitleLink' target="_blank" href=""><h1 id="projectTitle"></h1></a>
+                <a id='projectTitleLink' target="_blank" href=""><h1 id="projectTitle">{url ? url.toUpperCase() : 'Not Available'}</h1></a>
                 <div id="siteStats">
                     <div className="stat" id="signingProjectAddress">
-                        <h1 className="statTitle">Signing Address</h1> <a className = "data" id="signingAddress2Link" target="_blank" href=""><p id="signingAddress2">Not Available</p></a>
+                        <h1 className="statTitle">Signing Address</h1> <a className = "data" id="signingAddress2Link" target="_blank" href={"https://solscan.io/account/" + address}><p id="signingAddress2">{address.slice(0,10) + "... (" + addressName + ")"}</p></a>
                     </div>
                     <div className="stat">
 
-                        <h1 className="statTitle">Transaction Hash</h1> <a className = "data" id="transactionHashLink" target="_blank" href=""><p id="transactionHash">Not Available</p></a>
+                        <h1 className="statTitle">Transaction Hash</h1> <a className = "data" id="transactionHashLink" target="_blank" href={"https://solscan.io/tx/" + tx_hash}><p id="transactionHash">{tx_hash != '' ? tx_hash.slice(0,23) + "..." : 'Not Available'}</p></a>
                     </div>
                     <div className="stat">
 
-                        <h1 className="statTitle">Verified Web URL </h1> <a className = "data" id="verifiedWebURLLink" target="_blank" href=""><p id="verifiedWebURL">Not Available</p></a>
+                        <h1 className="statTitle">Verified Web URL </h1> <a className = "data" id="verifiedWebURLLink" target="_blank" href={url}><p id="verifiedWebURL">{url}</p></a>
                     </div>
                     <div className="stat">
-                        <h1 className="statTitle">Website Signed</h1> <a className = "data" id="websiteSignedLink" target="_blank" href=""><p id="websiteSigned">Not Available</p></a>
+                        <h1 className="statTitle">Website Signed</h1> <a className = "data" id="websiteSignedLink" target="_blank" href={url}><p id="websiteSigned">{url}</p></a>
                     </div>
                 </div>
             </div>
