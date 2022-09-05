46a776282051d8ffdf23542ad75786338922a1a1
Adrien <adriennavarre@hotmail.com>
2022-09-05 00:29:49 -0500

    starting settings page

--- hulio-extension/react-frontend/src/pages/settings/settings.tsx
+++ hulio-extension/react-frontend/src/pages/settings/settings.tsx
@@ -2,6 +2,45 @@ import name from '../images/name.svg'
 import './settings.css'
 
 export default function Settings(){
+
+    // Saves options to chrome.storage
+    function save_options() {
+        var warningCBox = document.getElementById('warningCBox').checked;
+        var retypingCBox = document.getElementById('retypingCBox').checked;
+        var tutorialsCBox = document.getElementById('tutorialsCBox').checked;
+        chrome.storage.local.set({
+        warningCBox: warningCBox,
+        retypingCBox: retypingCBox,
+        tutorialsCBox: tutorialsCBox,
+        }, function () {
+            // Update status to let user know options were saved.
+            var status = document.getElementById('status');
+            if(status) {
+                status.textContent = 'Options saved.';
+                setTimeout(function () {
+                    if(status) status.textContent = '';
+                }, 750);
+            }
+        });
+        
+        
+    }
+
+    // Restores select box and checkbox state using the preferences
+    // stored in chrome.storage.
+    function restore_options() {
+        // Use default value color = 'red' and likesColor = true.
+        chrome.storage.local.get({
+        warningCBox: true,
+        retypingCBox: true,
+        tutorialsCBox: true,
+        }, function (items) {
+        document.getElementById('warningCBox').checked = items.warningCBox;
+        document.getElementById('retypingCBox').checked = items.retypingCBox;
+        document.getElementById('tutorialsCBox').checked = items.tutorialsCBox;
+        });
+    }
+
     return(
         <div className="settings-container">
             <div className="content">
--- hulio-extension/react-frontend/src/pages/welcome/welcome.tsx
+++ hulio-extension/react-frontend/src/pages/welcome/welcome.tsx
@@ -1,6 +1,7 @@
 
 import './welcome.css'
 import name from '../images/name.svg'
+import { Link } from 'react-router-dom'
 
 export default function Welcome(){
     return (
@@ -11,9 +12,15 @@ export default function Welcome(){
                         <h1 id = "welcome">Welcome to hulio!</h1>
                         <p id = "introParagraph">We're your personal guide to the world of Solana. We provide everything from tutorials on how to use Solana dApps to security measures to keep you safe while exploring the ecosystem. Click below to get started!</p>
                         <div className= "buttons"> 
-                            <button id="tutorials">Tutorials</button>
-                            <button id="projects">Projects</button>
-                            <button id="configure">Configure</button>
+                            <Link to='/tutorials'>
+                                <button id="tutorials">Tutorials</button>
+                            </Link>
+                            <Link to='/home'>
+                                <button id="projects">Projects</button>
+                            </Link>
+                            <Link to='/settings'>
+                                <button id="configure">Configure</button>
+                            </Link>
                         </div>
                     </div>
                 </div>
